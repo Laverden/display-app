581f418aed2112adedbd20f6b4b5916c117a6424
Laverden <nlav05@gmail.com>
2021-07-16 13:07:11 +0200

    Updates app version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<HashRouter><App /></HashRouter>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
