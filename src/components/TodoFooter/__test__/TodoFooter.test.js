499aaf224f9275a8d7f4ca51e40674d2d2cc42f6
Laverden <nlav05@gmail.com>
2021-07-20 14:25:56 +0200

    Organizes test in describe blocks

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -12,42 +12,48 @@ const MockTodoFooter = ({numberOfIncompleteTasks}) => {
   )
 }
 
-it('should render the correct amount of incomplete tasks', () => {
-  render(
-    <MockTodoFooter 
-      numberOfIncompleteTasks={5}
-    />
-  );
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+describe("TodoFooter", () => {
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-    <MockTodoFooter 
-      numberOfIncompleteTasks={1}
-    />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+  it('should render the correct amount of incomplete tasks', () => {
+    render(
+      <MockTodoFooter 
+        numberOfIncompleteTasks={5}
+      />
+    );
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+  
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(
+      <MockTodoFooter 
+        numberOfIncompleteTasks={1}
+      />
+    );
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+  
+})
 
-it('should render TodoFooter and be visible to the user', () => {
-  render(
-    <MockTodoFooter 
-      numberOfIncompleteTasks={1}
-    />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeVisible();
-});
 
-it('should render TodoFooter with a paragraph element with value "1 task left"', () => {
-  render(
-    <MockTodoFooter 
-      numberOfIncompleteTasks={1}
-    />
-  );
-  const paragraphElement = screen.getByTestId("para");
-  expect(paragraphElement.textContent).toBe("1 task left");
-});
+
+// it('should render TodoFooter and be visible to the user', () => {
+//   render(
+//     <MockTodoFooter 
+//       numberOfIncompleteTasks={1}
+//     />
+//   );
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toBeVisible();
+// });
+
+// it('should render TodoFooter with a paragraph element with value "1 task left"', () => {
+//   render(
+//     <MockTodoFooter 
+//       numberOfIncompleteTasks={1}
+//     />
+//   );
+//   const paragraphElement = screen.getByTestId("para");
+//   expect(paragraphElement.textContent).toBe("1 task left");
+// });
